819682d70cebabfee89b009bf8b947dd156eda70
pgovekar <pranav.keshav.govekar@cern.ch>
2023-11-03 19:42:33 +0530

    fixed CFLAGS in build.tcl

--- scripts/build.tcl
+++ scripts/build.tcl
@@ -13,9 +13,9 @@ open_project HF_CSIM
 set_top algo_topIP1
 add_files ${TOPDIR}/src/algo_topIP1.h -cflags "-I$PATH_CMSWS -I${TOPDIR}/correlator-common/puppi/firmware" 
 add_files ${TOPDIR}/src/algo_top_IP.cpp -cflags "-I$PATH_CMSWS -I${TOPDIR}/correlator-common/puppi/firmware"
-add_files ${TOPDIR}/correlator-common/puppi/firmware/linpuppi.cpp -cflags "-I${TOPDIR}/correlator-common/CMSSW_12_5_2_patch1/src"
-add_files ${TOPDIR}/correlator-common/puppi/firmware/linpuppi.h -cflags "-I${TOPDIR}/correlator-common/CMSSW_12_5_2_patch1/src"
-add_files ${TOPDIR}/correlator-common/puppi/firmware/linpuppi_bits.h -cflags "-I${TOPDIR}/correlator-common/CMSSW_12_5_2_patch1/src"
+add_files ${TOPDIR}/correlator-common/puppi/firmware/linpuppi.cpp -cflags "-I$PATH_CMSWS"
+add_files ${TOPDIR}/correlator-common/puppi/firmware/linpuppi.h -cflags "-I$PATH_CMSWS"
+add_files ${TOPDIR}/correlator-common/puppi/firmware/linpuppi_bits.h -cflags "-I$PATH_CMSWS"
 
 add_files -tb ${TOPDIR}/testbench/algo_top_tb_IP.cpp -cflags "-I${TOPDIR}/src"
 open_solution "csim_solution" -flow_target vivado
